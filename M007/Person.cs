df633a20d85d1bf7fbf039e40760f79392a42c0b
ppedvLukasK <lukask@ppedv.de>
2022-05-18 14:11:41 +0200

    M007

---
+++ M007/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace M007
+{
+	public class Person
+	{
+		public string Name { get; set; }
+
+		public Person()
+		{
+			ZaehlePerson(); //statische Methode ist hier sichtbar da wir in der selben Klasse sind
+		}
+
+		~Person() //Destruktor: wird aufgerufen wenn der Garbage Collector das Objekt einsammelt
+		{
+			Console.WriteLine("Person eingesammelt");
+		}
+
+		public static int Zaehler = 0;
+
+		public static void ZaehlePerson()
+		{
+			Zaehler++;
+			//Name = "Max"; Nicht möglich da wir in einer statische Methode sind
+		}
+	}
+}
---
+++ M007/Program.cs
@@ -0,0 +1,43 @@
+﻿using M007;
+
+public class Program
+{
+	static void Main(string[] args)
+	{
+		for (int i = 0; i < 1000; i++)
+		{
+			Person p = new Person();
+			Person.ZaehlePerson(); //statische Methode aufrufen ohne Objekt (über Klassenname)
+		}
+
+		GC.Collect(); //Hier GC erzwingen
+		GC.WaitForPendingFinalizers(); //Warte auf alle Destruktoren
+
+		//Wertetyp
+		int original = 5;
+		int x = original;
+		original = 10;
+
+		//Referenztyp
+		Person p1 = new Person();
+		Person p2 = p1;
+		p1.Name = "Max";
+
+		//Wertetyp
+		string s1 = "Test";
+		string s2 = s1;
+		s1 = "Neuer Test";
+
+
+		int anzahl = 0;
+		Addiere(4, 2, ref anzahl);
+		Addiere(4, 3, ref anzahl);
+		Addiere(4, 4, ref anzahl);
+	}
+
+	public static void Addiere(int z1, int z2, ref int anz) //Referenz zur Variable
+	{
+		Console.WriteLine(z1 + z2);
+		anz++; //Anzahl Variable ansprechen und Wert erhöhen
+	}
+}
